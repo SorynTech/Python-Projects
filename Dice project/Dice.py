b7ea860dcd838df056db8542f6ba91acf5388899
Soryn Tech <zippydrawzstudioz@gmail.com>
2025-12-17 19:52:58 +0000

    Added the dice system with file checks

--- Dice project/Dice.py
+++ Dice project/Dice.py
@@ -1,5 +1,11 @@
 import os
+import random
+from fileinput import close
 
+badeffects=("give one coin to player next to you","no turn","reverse turn order","go back the amount of spaces you rolled")
+goodeffects=("take one coin","take another turn","go forwards +4","Roll for next player")
+goodeffect=random.choice(goodeffects)
+badeffect=random.choice(badeffects)
 def load_env_file(filepath='.env'):
     """Load variables from .env file into os.environ"""
     if not os.path.exists(filepath):
@@ -27,6 +33,63 @@ password = os.getenv('PASSWORD')
 
 if password:
     print("Password found!")
-    print(password)
 else:
     print("Password not found")
+#storing dicerolls
+open(f'dicerolls.txt', 'a').close()
+#DICE here
+def diceroll():
+    diceroll=random.randint(1,6)
+    goodchance=random.randint(1,100)
+    badchance=random.randint(1,100)
+    gchance=str(goodchance)
+    bchance=str(badchance)
+    droll=str(diceroll)
+    with open('dicerolls.txt', 'a') as f:
+        f.write(f"good chance: {gchance} \n Bad chance:{bchance} \n final roll: \n {droll}")
+        open(f'dicerolls.txt', 'a').close()
+    if goodchance>badchance:
+        diceroll=diceroll+1
+        print(diceroll)
+        print(goodeffect)
+    if badchance>goodchance:
+        diceroll=diceroll-1
+        print(diceroll)
+        print(badeffect)
+def gooddice():
+    passwordprotection=input("What is the password")
+    if passwordprotection != password:
+        print("Passwords do not match")
+        user_input = input("Would you Like to Roll the Random, Good Or bad dice")
+    else:
+        print(goodeffect)
+        diceroll=random.randint(1,6)
+        diceroll=diceroll+1
+        droll=str(diceroll)
+        with open('dicerolls.txt', 'a') as f:
+            f.write(f"final roll: \n {droll}")
+        open(f'dicerolls.txt', 'a').close()
+
+def baddice():
+    passwordprotection=input("What is the password")
+    if passwordprotection != password:
+        print("Passwords do not match")
+    else:
+        print(badeffect)
+        diceroll=random.randint(1,6)
+        diceroll=diceroll-1
+        droll=str(diceroll)
+        with open('dicerolls.txt', 'a') as f:
+            f.write(f"final roll: \n {droll}")
+        open(f'dicerolls.txt', 'a').close()
+
+user_input=input("Would you Like to Roll the Random, Good Or bad dice")
+if user_input == "Random":
+    diceroll()
+    exit()
+if user_input == "Good":
+    gooddice()
+    exit()
+else:
+    baddice()
+    exit()
